589b7599fa10cf5fe5021f34525dd5b7b66d2c99
Andrew Rubino <arubino322@gmail.com>
2025-05-12 22:06:17 -0400

    what is happening

--- app/app.py
+++ app/app.py
@@ -107,112 +107,111 @@ def get_collision_data(client, end_date):
 
 def main():
     # Initialize BigQuery Client
-    st.write("Hello world")) 
-    # client = init_bigquery()
+    client = init_bigquery()
 
-    # # Date filter widgets
-    # st.sidebar.header("Filter by Date Range")
-    # start_date = st.sidebar.date_input("Start Date", "2025-01-01")
-    # # have data only up until 60 days ago
-    # end_date = st.sidebar.date_input("End Date", datetime.today() - timedelta(days=61))
+    # Date filter widgets
+    st.sidebar.header("Filter by Date Range")
+    start_date = st.sidebar.date_input("Start Date", "2025-01-01")
+    # have data only up until 60 days ago
+    end_date = st.sidebar.date_input("End Date", datetime.today() - timedelta(days=61))
     
-    # st.title("NYC Pedestrian Collisions")
-    # st.subheader(f"Showing most recent data as of {end_date}")
+    st.title("NYC Pedestrian Collisions")
+    st.subheader(f"Showing most recent data as of {end_date}")
 
-    # # Ensure start_date is before end_date
-    # if start_date > end_date:
-    #     st.error("Error: End date must fall after start date.")
-    # else:
-    #     ##### MOST RECENT DAY DATA #####
-    #     last_day = get_most_recent_day(client, end_date)
+    # Ensure start_date is before end_date
+    if start_date > end_date:
+        st.error("Error: End date must fall after start date.")
+    else:
+        ##### MOST RECENT DAY DATA #####
+        last_day = get_most_recent_day(client, end_date)
         
-    #     # tiles at the top
-    #     col1, col2, col3 = st.columns(3)
-    #     col1.metric("Actual", int(last_day['crashes'][0]), border=True)
-    #     col2.metric("Predicted", int(last_day['yhat'][0]), border=True)
-    #     col3.metric("Delta %", f"{last_day['delta'][0]}%", border=True)
+        # tiles at the top
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Actual", int(last_day['crashes'][0]), border=True)
+        col2.metric("Predicted", int(last_day['yhat'][0]), border=True)
+        col3.metric("Delta %", f"{last_day['delta'][0]}%", border=True)
         
-    #     ###### TIME SERIES #####
-    #     df = get_collision_ts(client, start_date, end_date)
+        ###### TIME SERIES #####
+        df = get_collision_ts(client, start_date, end_date)
 
-    #     # Create the figure using Plotly graph_objects
-    #     fig = go.Figure()
+        # Create the figure using Plotly graph_objects
+        fig = go.Figure()
 
-    #     # Add trace for collisions
-    #     fig.add_trace(go.Scatter(
-    #         x=pd.to_datetime(df['crash_date']),
-    #         y=df['crashes'],
-    #         mode='lines',
-    #         name='Actual',
-    #         line=dict(color='orange', width=2)
-    #     ))
+        # Add trace for collisions
+        fig.add_trace(go.Scatter(
+            x=pd.to_datetime(df['crash_date']),
+            y=df['crashes'],
+            mode='lines',
+            name='Actual',
+            line=dict(color='orange', width=2)
+        ))
         
-    #     # Trace for UB & LB
-    #     fig.add_trace(go.Scatter(
-    #         x=pd.to_datetime(df['crash_date']),
-    #         y=df['yhat'],
-    #         mode='lines+markers',
-    #         name='Predicted',
-    #         line=dict(color='blue', width=2)
-    #     ))
+        # Trace for UB & LB
+        fig.add_trace(go.Scatter(
+            x=pd.to_datetime(df['crash_date']),
+            y=df['yhat'],
+            mode='lines+markers',
+            name='Predicted',
+            line=dict(color='blue', width=2)
+        ))
 
-    #     # upper bound
-    #     fig.add_trace(go.Scatter(
-    #         x=pd.to_datetime(df['crash_date']),
-    #         y=df['upper_bound'],
-    #         mode='lines',
-    #         name='Upper Bound',
-    #         line=dict(color='gray', width=1, dash='dash')
-    #     ))
+        # upper bound
+        fig.add_trace(go.Scatter(
+            x=pd.to_datetime(df['crash_date']),
+            y=df['upper_bound'],
+            mode='lines',
+            name='Upper Bound',
+            line=dict(color='gray', width=1, dash='dash')
+        ))
         
-    #     fig.add_trace(go.Scatter(
-    #         x=pd.to_datetime(df['crash_date']),
-    #         y=df['lower_bound'],
-    #         mode='lines',
-    #         name='Lower Bound',
-    #         line=dict(color='gray', width=1, dash='dash')
-    #     ))
+        fig.add_trace(go.Scatter(
+            x=pd.to_datetime(df['crash_date']),
+            y=df['lower_bound'],
+            mode='lines',
+            name='Lower Bound',
+            line=dict(color='gray', width=1, dash='dash')
+        ))
 
-    #     # Customize the layout of the plot
-    #     fig.update_layout(
-    #         title="NYC Pedestrian Collisions and Accidents Over Time",
-    #         xaxis_title="Date",
-    #         yaxis_title="Count",
-    #         yaxis=dict(
-    #             autorange=True,
-    #             autorangeoptions=dict(
-    #                 minallowed=0
-    #             )
-    #         ),
-    #         legend_title="Metrics",
-    #         hovermode="x unified"  # Combine hover info for both traces
-    #     )
+        # Customize the layout of the plot
+        fig.update_layout(
+            title="NYC Pedestrian Collisions and Accidents Over Time",
+            xaxis_title="Date",
+            yaxis_title="Count",
+            yaxis=dict(
+                autorange=True,
+                autorangeoptions=dict(
+                    minallowed=0
+                )
+            ),
+            legend_title="Metrics",
+            hovermode="x unified"  # Combine hover info for both traces
+        )
 
-    #     # Display the plot
-    #     st.plotly_chart(fig, use_container_width=True)
+        # Display the plot
+        st.plotly_chart(fig, use_container_width=True)
         
-    #     ##### MAP #####
-    #     map_df = get_collision_data(client, end_date)
+        ##### MAP #####
+        map_df = get_collision_data(client, end_date)
 
-    #     # plotly map
-    #     fig3 = px.scatter_map(
-    #         map_df,
-    #         title=f"Crashes on {end_date}",
-    #         lat="latitude",
-    #         lon="longitude",
-    #         size="people_involved",
-    #         size_max=10,
-    #         zoom=9,
-    #         color_discrete_sequence=["black"],
-    #         opacity=0.5,
-    #         hover_data={"people_involved": True, "latitude": False, "longitude": False}
-    #     )
-    #     fig3.update_layout(
-    #         mapbox_style='open-street-map',
-    #         width=800,
-    #         height=600
-    #     )
-    #     st.plotly_chart(fig3, on_select="rerun", selection_mode="points")
+        # plotly map
+        fig3 = px.scatter_map(
+            map_df,
+            title=f"Crashes on {end_date}",
+            lat="latitude",
+            lon="longitude",
+            size="people_involved",
+            size_max=10,
+            zoom=9,
+            color_discrete_sequence=["black"],
+            opacity=0.5,
+            hover_data={"people_involved": True, "latitude": False, "longitude": False}
+        )
+        fig3.update_layout(
+            mapbox_style='open-street-map',
+            width=800,
+            height=600
+        )
+        st.plotly_chart(fig3, on_select="rerun", selection_mode="points")
 
 # Run the app
 if __name__ == '__main__':
